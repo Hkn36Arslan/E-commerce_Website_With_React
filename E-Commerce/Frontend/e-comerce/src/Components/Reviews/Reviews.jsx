4d2f3f0551f77f58248c31c43cc38c3aa491ad23
Hakan Arslan <128188577+Hkn36Arslan@users.noreply.github.com>
2024-08-30 13:35:35 +0300

    BlogDetails sayfasındaki Reviews kısmı için ayrı bir component oluşturuldu ve bu Rivews componenti içerisinde  de ReviewItem ve ReviewForm olarak iki ayrı component oluşturuldu.

---
+++ E-Commerce/Frontend/e-comerce/src/Components/Reviews/Reviews.jsx
@@ -0,0 +1,54 @@
+import ReviewForm from "./ReviewForm";
+import ReviewItem from "./ReviewItem";
+import "./Reviews.css";
+
+const Reviews = () => {
+  return (
+    <div className="tab-panel-reviews">
+      <h3>2 reviews for Basic Colored Sweatpants With Elastic Hems</h3>
+      <div className="comments">
+        <ol className="comment-list">
+          <li className="comment-item">
+            <div className="comment-avatar">
+              <img src="img/avatars/avatar1.jpg" alt="" />
+            </div>
+            <div className="comment-text">
+              <ul className="comment-star">
+                <li>
+                  <i className="bi bi-star-fill"></i>
+                </li>
+                <li>
+                  <i className="bi bi-star-fill"></i>
+                </li>
+                <li>
+                  <i className="bi bi-star-fill"></i>
+                </li>
+                <li>
+                  <i className="bi bi-star-fill"></i>
+                </li>
+                <li>
+                  <i className="bi bi-star-fill"></i>
+                </li>
+              </ul>
+              <div className="comment-meta">
+                <strong>admin</strong>
+                <span>-</span>
+                <time>April 23, 2022</time>
+              </div>
+              <div className="comment-description">
+                <p>
+                  Sed perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium.
+                </p>
+              </div>
+            </div>
+          </li>
+          <ReviewItem />
+        </ol>
+      </div>
+      <ReviewForm />
+    </div>
+  );
+};
+
+export default Reviews;
